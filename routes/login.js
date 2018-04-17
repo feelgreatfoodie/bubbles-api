21453f06f9117d1da11eaacc03864715de437b4e
Alicia Key <alicia.key@galvanize.com>
2018-04-17 13:14:50 -0600

    Username / password authentication works.

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
+const bcrypt = require('bcryptjs')
 
 router.post('/', (req, res, next) => {
-  const { username, password } = req.body
-  console.log(`>>> ${username} ${password}`)
+  const { name, password } = req.body
+  knex('users')
+    .where('name', name)
+    .then(result => {
+      if (result.length !== 1) {
+        res.status(400).send('Bad username')
+      }
+      else if (bcrypt.compareSync(password, result[0].password)) {
+        res.sendStatus(200)
+      }
+      else {
+        res.status(400).send('Bad password')
+      }
+    })
 })
 
 module.exports = router
